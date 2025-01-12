6cf60f61c951852aef3203c4330b244014a887d2
Joris S <100357138+Jorre05@users.noreply.github.com>
2025-01-12 13:34:34 +0100

    Update named.sh

--- named.sh
+++ named.sh
@@ -6,16 +6,16 @@ set -m
 echo "Git clone ${GITHUB_CONFIG_REPO} in ${GITHUB_CLONE_DIR}"
 rm -fr /etc/named/*
 git clone ${GITHUB_CONFIG_REPO} ${GITHUB_CLONE_DIR}
-cp ${GITHUB_CLONE_DIR}/dns/* /etc/named
+cp -frp ${GITHUB_CLONE_DIR}/dns/* /etc/named
 
 if [ ! -d /tmp/cache ]; then
-    bashio::log.info "Creating cache folder"
+    echo "Creating cache folder"
     mkdir -p /tmp/cache
     chown named:named /tmp/cache
     chmod 755 /tmp/cache
 fi
 
-/usr/sbin/named -g -c /config/named.conf -u named &
+/usr/sbin/named -g -c /etc/named/named.conf -u named &
 
 # now we bring the primary process back into the foreground
 # and leave it there
