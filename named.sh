a04a21ace71006d93c5ebef08aec0a4556366d88
Joris S <100357138+Jorre05@users.noreply.github.com>
2025-01-12 13:47:23 +0100

    Update named.sh

--- named.sh
+++ named.sh
@@ -6,12 +6,22 @@ set -m
 echo "Git clone ${GITHUB_CONFIG_REPO} in ${GITHUB_CLONE_DIR}"
 rm -fr /etc/named/*
 git clone ${GITHUB_CONFIG_REPO} ${GITHUB_CLONE_DIR}
+echo "Lijst in clone dir"
 ls -l ${GITHUB_CLONE_DIR}
+
+echo "Lijst in clone dir dns"
 ls -l ${GITHUB_CLONE_DIR}/dns/*
+
+echo "kopie naar named"
 cp -frp ${GITHUB_CLONE_DIR}/dns/* /etc/named/
-ls -l /etc/named
+
+echo "lijst in dnamed"
+ls -l /etc/named/*
 chown -R named:named /etc/named
 chmod -R 755 /etc/named
+
+echo "na ch stuff"
+ls -l /etc/named/named.conf
 
 if [ ! -d /tmp/cache ]; then
     echo "Creating cache folder"
