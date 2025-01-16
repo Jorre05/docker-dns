fe4437aedfc96517fcad3859530f6045c993202a
Joris S <100357138+Jorre05@users.noreply.github.com>
2025-01-16 13:37:11 +0100

    Update named.sh

--- named.sh
+++ named.sh
@@ -3,23 +3,33 @@
 # turn on bash's job control
 set -m
 
-mkdir /config
+if [ ! -d /${GITHUB_CLONE_DIR} ]; then
+    echo "Aanmaken git map "${GITHUB_CLONE_DIR}
+    mkdir -p /${GITHUB_CLONE_DIR}
+    git clone ${GITHUB_CONFIG_REPO} ${GITHUB_CLONE_DIR}
+else
+    echo "git pull in map "${GITHUB_CLONE_DIR}
+    cd ${GITHUB_CLONE_DIR}
+    git pull
+fi
+
+if [ ! -d /config ]; then
+    echo "Aanmaken config map "
+    mkdir -p /config
+    chown named:named /config
+    chmod 755 /config
+fi
+
+if [ ! -f /config/named.conf ]; then
+    echo "kopie naar config"
+    cp -frp ${GITHUB_CLONE_DIR}/dns/* /config
+    sed s/#LISTEN_ON#/${LISTEN_ON}/g /config/named.conf -i
+fi
+
 mkdir -p /data/cache
 chown named:named /data/cache
 chmod 755 /data/cache
-    
-echo "Git clone ${GITHUB_CONFIG_REPO} in ${GITHUB_CLONE_DIR}"
-git clone ${GITHUB_CONFIG_REPO} ${GITHUB_CLONE_DIR}
-
-echo "kopie naar config"
-cp -frp ${GITHUB_CLONE_DIR}/dns/* /config
-sed s/#LISTEN_ON#/${LISTEN_ON}/g /config/named.conf -i
-
-chown -R named:named /config
-chmod -R 755 /config
 
 /usr/sbin/named -g -c /config/named.conf -u named &
 
-# now we bring the primary process back into the foreground
-# and leave it there
 fg %1
